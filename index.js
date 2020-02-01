6785ca525b83ae89e0c458228aff0059712ef7f4
Randy <randyisbusy@gmail.com>
2020-01-31 23:05:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution here!
+// Write your solution here! 
+
+
+let append = ["Milo", "Otis", "Garfield"] 
+
+let prepend = ["Milo", "Otis", "Garfield"] 
+
+let removeLast = ["Milo", "Otis", "Garfield"]
+ 
+let removeFirst =  ["Milo", "Otis", "Garfield"] 
+
+let newAppend = append.push("Odie") 
+
+let newPrepend = prepend.unshift("Odie") 
+ 
+let newRemoveLast = removeLast.pop() 
+
+let newRemoveFirst = removeFirst.shift()
